9536c6517ae9702c46c16838ac407eb02682c746
Kajetan Jonak <kajetan@jonak.org.pl>
2020-04-17 20:31:37 +0200

    thread implementation

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
-import sys
-from PyQt5 import QtWidgets, uic, QtGui
+import sys, time
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+
 
 from mainwindow import Ui_MainWindow
 
+
+class telemetryThread(QtCore.QThread):   
+    
+    dataReceived = QtCore.pyqtSignal(object)
+    def __init__(self, parent = None):
+       super(telemetryThread, self).__init__(parent)
+
+    def run(self):  #here we should use class for odroid connection. 
+        a= 50
+        while 1:
+            a+=1
+            if a>60:
+                a=50
+            time.sleep(0.3)
+            data=(a,1)
+            self.dataReceived.emit(data)
+
+    def sendData(self, data=()):
+        pass
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow,self).__init__()
         self.setupUi(self)
         self.setStyleSheet(open('style/mainWindow.css').read())
+        self.telemetryThread=telemetryThread()
+        self.connectionBar.b_connect.pressed.connect(self.manageConnection)
+        
 
-    def getControl(self):
-        return self.connectionBar
+    def manageConnection(self):
+        if self.telemetryThread.isRunning():
+            self.telemetryThread.dataReceived.disconnect()
+            self.telemetryThread.terminate()
+        else:
+            
+            self.telemetryThread.start()
+            self.telemetryThread.dataReceived.connect(self.updateWidgets)
+            self.connectionBar.sendData.connect(self.telemetryThread.sendData)
+
+    def updateWidgets(self, data):
+        hum = data[0]
+        
+        self.connectionBar.update(hum)
 
 
-def run():
+def main():
     app=QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    control = window.getControl()
-    
     app.exec_()
 
-run()
+main()
--- widgets/connectionBar.py
+++ widgets/connectionBar.py
@@ -1,14 +1,19 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from .connectionBar_ui import Ui_connectionBar
 
-#Creating child class of Ui_connectionBar and qwidget
+
 class connectionBar(QtWidgets.QWidget,Ui_connectionBar):
+    #if widget send something to odroid you must create signal to comunicate with connection thread
+    sendData = QtCore.pyqtSignal(object)    #the argument can be anything -> 'int', 'QString'... object is for passing python objects
+
     def __init__(self,parent=None):
        QtWidgets.QWidget.__init__(self,parent)
        self.setupUi(self)       
+
 #now you can add labels stylesheets... whatever you need
        self.b_connect.pressed.connect(self.b_connectAction)         
        self.setStyleSheet(open('./style/connectionBar.css').read())
+
     def b_connectAction(self):
             if self.b_connect.text()=="Connect":
                 self.b_connect.setText("Disconnect")
@@ -18,12 +23,18 @@ class connectionBar(QtWidgets.QWidget,Ui_connectionBar):
                 self.b_connect.setText("Connect")
                 self.ip_text.setEnabled(True)
 
-    def update(self):
-        self.show()
+    def update(self, humidity):
+        val ="humidity: "
+        val+=str(humidity)
+        self.l_connection.setText(val)
+
+#just provide data, and attach this function to send button
+    def send(self, data=()):
+        self.sendData.emit(data)
 
    
 
-#każdy widget może działać jako osobne okno -> super do testowania. odkomentuj kod poniżej i uruchom...
+#Each widget can work like an standalone app. Just uncomment the code bellow and remove dot from import function.
 #import sys
 #app=QtWidgets.QApplication(sys.argv)
 #window = connectionBar()
